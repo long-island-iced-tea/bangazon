08fdf1d7d0b319499fba892e3110840bde9d05cf
michael <michael.clark1992@gmail.com>
2018-11-10 17:43:24 -0600

    setup storage and controller

---
+++ BangazonInc/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonInc.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonInc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        DatabaseInterface _db;
+        CustomerStorage _customers;
+
+
+        public CustomersController(DatabaseInterface db)
+        {
+            _db = db;
+            _customers = new CustomerStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (var db = _db.GetConnection())
+            {
+                return Ok(db.Database);
+            }
+        }
+    }
+}
---
+++ BangazonInc/DataAccess/CustomerStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.DataAccess
+{
+    public class CustomerStorage
+    {
+        DatabaseInterface _db;
+
+        public CustomerStorage(DatabaseInterface db)
+        {
+            _db = db;
+        }
+    }
+}
