8927e2edc090925043f91ea4e07acd0618549a7d
michael <michael.clark1992@gmail.com>
2018-11-10 12:05:18 -0600

    populates payment types

---
+++ BangazonInc/Database/201811101204PopulatePaymentType.sql
@@ -0,0 +1,24 @@
+DECLARE @counter int = 1
+DECLARE @type varchar(100)
+
+WHILE @counter <= 1000
+BEGIN
+
+	SET @type = 'Debit'
+	IF @counter % 3 = 0 SET @type = 'AmEx'
+	IF @counter % 4 = 0 SET @type = 'Mastercard'
+	IF @counter % 5 = 0 SET @type = 'Visa'
+
+	INSERT INTO [dbo].[PaymentType]
+           ([id]
+           ,[customerId]
+           ,[accountNum]
+           ,[type])
+     VALUES
+           (@counter
+           ,@counter
+           ,@counter + 1000000000000000
+           ,@type)
+
+	SET @counter = @counter + 1
+END
