fbcd00b729012e3a38bb0687a412ff7ef10f861e
Amanda Slayton <alslayton86@gmail.com>
2018-11-10 12:38:27 -0600

    sql to populate orders

---
+++ BangazonInc/Database/201811101311PopulateProducts.sql
@@ -0,0 +1,25 @@
+/*Products*/
+
+DECLARE @count int;
+DECLARE @customerCount int;
+DECLARE @deptCount int;
+SET @count = 1;
+SET @customerCount = 1;
+SET @deptCount = 0;
+
+while @count <= 5000
+  BEGIN 
+  if @customerCount > 1000
+    SET @customerCount = 1;
+  if  @deptCount > 100
+    SET @deptCount = 0;
+  
+
+  INSERT INTO Products (id, Name, description, price, quantity, customerId, productType)
+    Values(@count, 'Product' + LTrim(str(@count)), 'This is a description about product ' + LTrim(str(@count)), 9.99, 1, @customerCount, 'type' + lTrim(str(@deptCount)));
+
+    SET @count = @count + 1;
+	SET @customerCount = @customerCount + 1;
+	SET @deptCount = @deptCount + 1;
+  CONTINUE;
+END
