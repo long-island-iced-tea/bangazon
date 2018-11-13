2d3837e8dc0dd8ce6c49171f6c06aa31aa32274e
Amanda Slayton <alslayton86@gmail.com>
2018-11-12 18:54:20 -0600

    added PaymentType storage, model, controller

---
+++ BangazonInc/Models/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.Models
+{
+    public class PaymentType
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int AccountNum { get; set; }
+        public string Type { get; set; }
+    }
+}
