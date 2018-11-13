9e64c71b440c9bfe993efc667f6547c50fce4c43
John Achor <johnachor@gmail.com>
2018-11-12 19:25:12 -0600

    Get all orders endpoint functional

---
+++ BangazonInc/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BangazonInc.DataAccess;
+
+namespace BangazonInc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        DatabaseInterface _db;
+        OrderAccess _oa;
+
+        public OrdersController(DatabaseInterface db)
+        {
+            _db = db;
+            _oa = new OrderAccess(db);
+        }
+
+        // GET /api/orders
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(_oa.GetAllOrders());
+        }
+    }
+}
---
+++ BangazonInc/DataAccess/OrderAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using BangazonInc.Models;
+
+
+namespace BangazonInc.DataAccess
+{
+    public class OrderAccess
+    {
+        private DatabaseInterface _db;
+
+        public OrderAccess (DatabaseInterface db)
+        {
+            _db = db;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            using (var sql = _db.GetConnection())
+            {
+                return sql.Query<Order>("SELECT * FROM Orders").ToList();
+            }
+        }
+    }
+}
