ba8f3352917a88c5616f6c559ac8a6d82e2f0836
Stix <jessica.rodgers11@gmail.com>
2018-11-12 20:31:46 -0600

    added model controller and storage'

---
+++ BangazonInc/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonInc.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonInc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        DatabaseInterface _db;
+        ProductStorage _product;
+
+        public ProductController(DatabaseInterface db)
+        {
+            _db = db;
+            _product = new ProductStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var allProducts = _product.GetProduct();
+            return Ok(allProducts);
+        }
+    }
+}
---
+++ BangazonInc/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set;}
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductType { get; set; }
+    }
+}
