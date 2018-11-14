58553237bf60b00d4846ac1e1f09a6a970f1f7f7
Amanda Slayton <alslayton86@gmail.com>
2018-11-13 20:44:55 -0600

    All employees returned

--- BangazonInc/Controllers/EmployeeController.cs
+++ BangazonInc/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonInc.DataAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,20 @@ namespace BangazonInc.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        
+        DatabaseInterface _db;
+        EmployeeStorage _Employees;
+
+        public EmployeeController(DatabaseInterface db)
+        {
+            _db = db;
+            _Employees = new EmployeeStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allPayType = _Employees.GetAllEmployees();
+            return Ok(allPayType);
+        }
     }
 }
--- BangazonInc/DataAccess/EmployeeStorage.cs
+++ BangazonInc/DataAccess/EmployeeStorage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BangazonInc.Models;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,24 @@ namespace BangazonInc.DataAccess
 {
     public class EmployeeStorage
     {
+        DatabaseInterface _db;
+
+        public EmployeeStorage(DatabaseInterface db)
+        {
+            _db = db;
+        }
+
+        /******************************
+         Get All Employees
+         ******************************/
+        public List<Employees> GetAllEmployees()
+        {
+            using (var db = _db.GetConnection())
+            {
+                string sql = "SELECT * FROM Employees";
+                return db.Query<Employees>(sql).ToList();
+            }
+
+        }
     }
 }
