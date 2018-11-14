3fc6727591d0005ad3a0a20860e6aa4b064cb2a9
Amanda Slayton <alslayton86@gmail.com>
2018-11-13 20:35:17 -0600

    setup controller, model, storage for employees

---
+++ BangazonInc/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public int ComputerId { get; set; }        
+    }
+}
