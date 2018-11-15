83550f40c0ac2ffb2cdd8bfc1ce41ee4e617aeea
michael <michael.clark1992@gmail.com>
2018-11-15 11:25:01 -0600

    setup model, controller, and storage

---
+++ BangazonInc/Controllers/ProductTypesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonInc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        DatabaseInterface _db;
+
+        public ProductTypesController(DatabaseInterface db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BangazonInc/DataAccess/ProductTypeStorage.cs
@@ -0,0 +1,20 @@
+﻿using BangazonInc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.DataAccess
+{
+    public class ProductTypeStorage
+    {
+        DatabaseInterface _db;
+        static List<ProductType> _productTypes;
+
+        public ProductTypeStorage(DatabaseInterface db)
+        {
+            _db = db;
+            _productTypes = new List<ProductType>();
+        }
+    }
+}
