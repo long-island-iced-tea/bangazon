6f79a62efc590bff6a40c277d57b7428e6b8c384
Stix <jessica.rodgers11@gmail.com>
2018-11-17 10:12:30 -0600

    getting all departments

---
+++ BangazonInc/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonInc.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonInc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        DatabaseInterface _db;
+        DepartmentStorage _department;
+
+        public DepartmentController (DatabaseInterface db)
+        {
+            _db = db;
+            _department = new DepartmentStorage(db);
+        }
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allDepts = _department.GetDept();
+            return Ok(allDepts);
+        }
+
+         
+    }
+}
---
+++ BangazonInc/DataAccess/DepartmentStorage.cs
@@ -0,0 +1,30 @@
+﻿using BangazonInc.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.DataAccess
+{
+    public class DepartmentStorage
+    {
+        DatabaseInterface _db;
+
+        public DepartmentStorage (DatabaseInterface db)
+        {
+            _db = db;
+        }
+
+        // Get All Departments
+
+        public List<Department> GetDept()
+        {
+          using(var db = _db.GetConnection())
+            {
+                string sql = "Select * from Department";
+                return db.Query<Department>(sql).ToList();
+            }
+        }
+    }
+}
