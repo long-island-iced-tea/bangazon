825c5cced6691ac5e964c6acc04c85ff481243e6
John Achor <johnachor@gmail.com>
2018-12-07 13:44:33 -0600

    including employee id in computer post request assigns computer to employee

---
+++ BangazonInc/Models/ComputerWithEmployeeId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonInc.Models
+{
+    public class ComputerWithEmployeeId : Computer
+    {
+        public ComputerWithEmployeeId() { }
+
+        public ComputerWithEmployeeId(Computer computer)
+        {
+            if (computer.DecommissionedAt is null)
+            {
+                this.DecommissionedAt = null;
+            }
+            else
+            {
+                this.DecommissionedAt = computer.DecommissionedAt;
+            }
+            this.Id = computer.Id;
+            this.IsNew = computer.IsNew;
+            this.IsWorking = computer.IsWorking;
+            if (computer.PurchasedAt is null)
+            {
+                this.PurchasedAt = null;
+            }
+            else
+            {
+                this.PurchasedAt = computer.PurchasedAt;
+            }
+        }
+        
+        public int? EmployeeId { get; set; }
+    }
+}
