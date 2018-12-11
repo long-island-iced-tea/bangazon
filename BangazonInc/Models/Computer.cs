7114d5d773a06e37f413e1db3e968fd72bf30b38
michael <michael.clark1992@gmail.com>
2018-12-10 19:54:53 -0600

    gets data back for computers

--- BangazonInc/Models/Computer.cs
+++ BangazonInc/Models/Computer.cs
@@ -8,11 +8,11 @@ namespace BangazonInc.Models
     public class Computer
     {
         public int Id { get; set; }
-        public DateTime? purchasedAt { get; set; }
-        public DateTime? decommissionedAt { get; set; }
-        public DateTime createdAt { get; set; }
-        public bool isNew { get; set; }
-        public bool isWorking { get; set; }
+        public DateTime? PurchasedAt { get; set; }
+        public DateTime? DecommissionedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsWorking { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
     }
