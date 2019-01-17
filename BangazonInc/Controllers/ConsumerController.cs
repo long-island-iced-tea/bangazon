33ec7b40ee6f647eaee07d9e88fece70956faa0a
Amanda Slayton <alslayton86@gmail.com>
2019-01-16 20:33:47 -0600

    list endpoints of consumer controller

--- BangazonInc/Controllers/ConsumerController.cs
+++ BangazonInc/Controllers/ConsumerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonInc.DataAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,47 @@ namespace BangazonInc.Controllers
     [ApiController]
     public class ConsumerController : ControllerBase
     {
+        DatabaseInterface _db;
+        ProductStorage _product;
+
+
+        public ConsumerController(DatabaseInterface db)
+        {
+            _db = db;
+            _product = new ProductStorage(db);
+        }
+        //products GET: Gets all products
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+        //products/recent GET: Gets 20 recent products
+        [HttpGet("products/recent")]
+        public IActionResult GetRecent()
+        {
+            return Ok();
+        }
+
+        //products? q = GET: Search products, has q as a parameter
+        [HttpGet("products/")]
+        public IActionResult searchProducts()
+        {
+            return Ok();
+        }
+        //products/category GET: Gets products sorted by product type
+        [HttpGet("products/catagory/{id}")]
+
+        //GET: Gets product by Id
+        [HttpGet("products/{id}")]
+        //login GET: returns the authenticated user by uid
+        [HttpGet("{id}")]
+        public IActionResult getUser(int id) {
+            return Ok();
+            
+        }
+
+        //register GET: adds new customer with firebase uid
+        //order POST adds new order and productorders
     }
 }
