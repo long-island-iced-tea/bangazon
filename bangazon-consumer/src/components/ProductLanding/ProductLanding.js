938b8d5c0323b69516ca742d6488ab431970bc16
michael <michael.clark1992@gmail.com>
2019-01-19 10:07:21 -0600

    added link to product card

--- bangazon-consumer/src/components/ProductLanding/ProductLanding.js
+++ bangazon-consumer/src/components/ProductLanding/ProductLanding.js
@@ -16,31 +16,37 @@ class ProductLanding extends React.Component {
     // provides dummy data, will replace with api call
     const products = [
       {
+        id: 1,
         name: 'Super cool product',
         category: 'Cat1',
         price: 2.99,
       },
       {
+        id: 2,
         name: 'Another awesome product',
         category: 'Category1',
         price: 9.99,
       },
       {
+        id: 3,
         name: 'Yet another product',
         category: 'categoryyyyy',
         price: 0.83
       },
       {
+        id: 4,
         name: 'Wowza',
         category: 'cattt',
         price: 0.99,
       },
       {
+        id: 5,
         name: 'Long-named Product for your purchasing requirements',
         category: 'Category1',
         price: 4.99,
       },
       {
+        id: 6,
         name: 'Yet another product',
         category: 'categoryyyyy',
         price: 2.01
