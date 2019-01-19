e7122b6d603071c98d0a8d2329a0d43da904d8c1
John Achor <johnachor@gmail.com>
2019-01-18 22:19:19 -0600

    initial firebase auth methods working

---
+++ bangazon-consumer/src/firebase/init.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase';
+import constants from '../constants.json';
+
+const firebaseApp = () => {
+    firebase.initializeApp(constants.firebaseConfig);
+};
+
+export default firebaseApp;
