3783def97a8744f97cb795355e91b95afc167c8d
Stix <jessica.rodgers11@gmail.com>
2019-01-19 10:12:04 -0600

    footer sticking

--- bangazon-consumer/src/app/App.js
+++ bangazon-consumer/src/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Navbar from '../components/Navbar/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 import LoginForm from '../components/LoginForm/LoginForm';
 import RegisterForm from '../components/RegisterForm/RegisterForm';
 import ProductLanding from '../components/ProductLanding/ProductLanding';
@@ -12,7 +13,8 @@ firebase.init();
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App Site">
+      <div className="Site-content">
         <Navbar/>
         <BrowserRouter>
           <Switch>
@@ -21,6 +23,7 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
           </Switch>
         </BrowserRouter>
+        </div>
         <Footer/>
       </div>
     );
--- bangazon-consumer/src/components/Footer/Footer.js
+++ bangazon-consumer/src/components/Footer/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
-//import './Footer.css';
+ import Link from 'react-router-dom';
+import './Footer.scss';
 
 class Footer extends React.Component {
     render () {
         return (
             <div>
-                <h1>Footer</h1>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <h5> &copy; Bangazon | 2019</h5>
+                </div>
+                </nav>
             </div>
         );
     }
 }
-
 export default Footer;
